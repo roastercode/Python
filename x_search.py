bfdabe7c58f095a6570222f8f6c19bd93e1aeded
XL04D <aurelien@xload.io>
2013-10-19 22:52:27 +0200

    Search for the words you wish in a file

---
+++ x_search.py
@@ -0,0 +1,19 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+# License   : GNU GPL v3 or later
+# Author    : XL04D
+# Mail      : aurelien@xload.IO
+# Project   : X search
+# replace the X to search your wish
+
+import fileinput, re
+pat = re.compile(r'[a-z\-\.]+X[a-z\-\.]+', re.IGNORECASE)
+files = set()
+for line in fileinput.input():
+    for X in pat.findall(line):
+        files.add(X)
+    for X in sorted(files):
+        print X
+        
+
+# $ python x_search.py <file>
