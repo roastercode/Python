31c7985df846941336ac7cec41638d9b31690566
xl04d <aurelien@xload.io>
2014-03-23 11:54:48 +0100

    ButtonPlace.py

---
+++ ButtonPlace.py
@@ -0,0 +1,26 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+# License   : GNU GPL v3 or later
+# Author    : aurelien desbrieres
+# Mail      : aurelien@hackers.guru
+# Project   : ButtonPlace
+
+import wx
+
+app = wx.App()
+win = wx.Frame(None, title="Simple Editor", size=(410, 335))
+win.Show()
+
+loadButton = wx.Button(win, label='0pen',
+                       pos=(225, 5), size=(80, 25))
+
+saveButton = wx.Button(win, label='Save',
+                       pos=(315, 5), size=(80, 25))
+
+filename = wx.TextCtrl(win, pos=(5, 5), size=(210, 25))
+
+contents = wx.TextCtrl(win, pos=(5, 35), size=(390, 260),
+                       style=wx.TE_MULTILINE | wx.HSCROLL)
+
+
+app.MainLoop()
