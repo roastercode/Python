94d346f462ac15f517f2eb1c961d3d4d4c5fddec
xl04d <aurelien@xload.io>
2014-03-23 11:20:35 +0100

    SimpleGui.py

---
+++ SimpleGui.py
@@ -0,0 +1,12 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+# License   : GNU GPL v3 or later
+# Author    : aurelien desbrieres
+# Mail      : aurelien@hackers.guru
+# Project   : SimpleGui
+
+import wx
+app = wx.App()
+win = wx.Frame(None)
+win.Show()
+app.MainLoop()
