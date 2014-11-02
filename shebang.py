260b9b0c5f548a03cb3e158c6509923492605499
Aurélien DESBRIÈRES <aurelien@hackers.camp>
2014-11-02 09:59:29 +0100

    shebang.py corrected model of shebang

---
+++ shebang.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# License    : GNU GPL v3 or later
+# Author     : Aurélien DESBRIERES
+# Mail       : aurelien@hackers.camp
+# Project    : Farenheit - Celcius table
+# Created on : Sun Oct  5 17:02:29 2014
+#
+# References
+#
+# including comments, declarations, variables, arithmetic expressions, loops and
+# formatted output
+#
+# Course material
+#
+# K & R
+#
+#
+# Project
+#
+# Here is my SheBang model for all my stuff
+#
+#
+
+
