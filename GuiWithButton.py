6112bccd12f1d5483a3c61807066df9afc09d4a2
Aurélien DESBRIÈRES <aurelien@hackers.camp>
2014-11-05 08:51:42 +0100

    GuiWithButton.py updated

--- GuiWithButton.py
+++ GuiWithButton.py
@@ -1,9 +1,17 @@
-#!/usr/bin python
-# -*- coding: utf-8 -*-
-# License   : GNU GPL v3 or later
-# Author    : aurelien desbrieres
-# Mail      : aurelien@hackers.guru
-# Project   : GuiWithButton
+#!/usr/bin/env python3
+# License    : GNU GPL v3 or later
+# Author     : Aurélien DESBRIERES
+# Mail       : aurelien@hackers.camp
+# Project    : GuiWithButton
+# Created on : 
+#
+# References
+#
+# python3 env
+#
+#
+# Course material
+#
 
 import wx
 app = wx.App()
