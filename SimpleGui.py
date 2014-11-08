369d8b7089fbf5a0ff941e7057095352a71484f0
Aurélien DESBRIÈRES <aurelien@hackers.camp>
2014-11-08 09:58:06 +0100

    SimpleGui.py updated

--- SimpleGui.py
+++ SimpleGui.py
@@ -1,9 +1,20 @@
-#!/usr/bin python
-# -*- coding: utf-8 -*-
-# License   : GNU GPL v3 or later
-# Author    : aurelien desbrieres
-# Mail      : aurelien@hackers.guru
-# Project   : SimpleGui
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+# License    : GNU GPL v3 or later
+# Author     : Aurélien DESBRIERES
+# Mail       : aurelien@hackers.camp
+# Project    : simplegui
+# Created on : Sat Nov 8 09:56:56 2014
+#
+# References
+#
+# python3 env
+# wx
+#
+# Course material
+#
+# 
+#
 
 import wx
 app = wx.App()
