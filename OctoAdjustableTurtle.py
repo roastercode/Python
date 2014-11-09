d9124618c2fcb2270f2840c7034e57ccd8189dce
xl04d <aurelien@xload.io>
2014-11-09 08:48:16 +0100

    OctoAdjustableTurtle.py corrected

--- OctoAdjustableTurtle.py
+++ OctoAdjustableTurtle.py
@@ -20,11 +20,18 @@
 
 import turtle
 
+
+def left_triangle(length):
+        for i in xrange(3):
+            turtle.forward(length)
+            turtle.left(110)
+
+
 def fractal_side(length, fractal, proportions):
     if fractal == 0:
         turtle.forward(length)
     else:
-        [l1, l2, l3, l4, l5, l6, l7, l8]= [p*length for p in proportions]
+        [l1, l2, l3, l4, l5, l6, l7, l8] = [p*length for p in proportions]
         fractal_side(l1, fractal-1, proportions)
         turtle.right(80)
         fractal_side(l2, fractal-1, proportions)
@@ -40,8 +47,15 @@ def fractal_side(length, fractal, proportions):
         fractal_side(l7, fractal-1, proportions)
         turtle.right(80)
         fractal_side(l8, fractal-1, proportions)
-        
+
+
+def fractal_triangle(length, fractal, proportions):
+        for i in xrange(3):
+            fractal_side(length, fractal, proportions)
+            turtle.left(110)
+
+
 turtle.reset()
-turtle.speed(8)
+turtle.speed(10)
 fractal_triangle(400, 3, (.3, .3, .3, .3, .3, .3, .3, .3))
 turtle.exitonclick()
