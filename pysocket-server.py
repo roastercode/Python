168d989163607d8d0a323cfd60394c33225e8ab7
k0d3x <aurelien@hackers.camp>
2014-11-16 10:17:28 +0100

    pysocket-server.py Just writed!

--- pysocket-server.py
+++ pysocket-server.py
@@ -23,3 +23,17 @@
 # Socket "Information Channel"
 #
 
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+port = 1234
+s.bind((host, port))
+
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print 'Connection from' , addr
+    c.send('Welcome on board')
+    c.close
