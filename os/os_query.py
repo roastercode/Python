b121e0b3b0166f9b01598bf5ded81c7911a33abd
k0d3x <aurelien@hackers.camp>
2014-11-16 07:44:38 +0100

    initial commit of os_query.py

---
+++ os/os_query.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# License    : GNU GPL v3 or later
+# Author     : Aurélien DESBRIERES
+# Mail       : aurelien@hackers.camp
+# Project    : Query DB
+# Created on : Sun Nov 16 07:43:00 2014
+#
+# Write with Emacs-Nox
+#
+# References
+#
+# import sqlite3 def sys
+#
+# Course material
+#
+# Python book - Own modification
+#
+
+#
+# OS Query is made to works with an existant DB or made
+# with importdb.py you can find at github.com/k0d3x
+#
+
