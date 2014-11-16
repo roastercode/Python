9e8a15bf5a3b0f80fdbd41097526a4f803503541
k0d3x <aurelien@hackers.camp>
2014-11-16 10:13:33 +0100

    pysocket-server.py first commit!

---
+++ pysocket-server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# License    : GNU GPL v3 or later
+# Author     : Aurélien DESBRIERES
+# Mail       : aurelien@hackers.camp
+# Project    : Socket Server
+# Created on : Sun Nov 16 10:09:19 2014
+#
+# Write with Emacs-Nox
+#
+# References
+#
+# socket gethostname
+#
+#
+# Course material
+#
+# Python book - Own modification
+#
+
+#
+# Minimal Python Socket Server
+# Socket "Information Channel"
+#
+
